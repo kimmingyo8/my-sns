bff7899972f5a836a03a653020d32247980cce61
kimmingyo8 <kimmingyo8@naver.com>
2023-10-10 03:57:38 +0900

    firebase 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import Router from 'routes/Router';
 import Layout from 'layouts/Layout';
 
+import { getAuth } from 'firebase/auth';
+import { app } from 'firebaseApp';
+import { useState } from 'react';
+
 function App() {
+  const auth = getAuth(app);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser,
+  );
+
   return (
     <Layout>
       <Router />
