4ee98a7fe5f3412a75f866ca1386c6706ed792bb
kimmingyo8 <kimmingyo8@naver.com>
2023-10-17 00:18:26 +0900

    디테일 페이지 작업

--- src/pages/posts/detail.tsx
+++ src/pages/posts/detail.tsx
@@ -1,5 +1,41 @@
+import Loader from 'components/loader/Loader';
+import PostBox from 'components/post/PostBox';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from 'firebaseApp';
+import { PostProps } from 'pages/home';
+import { useCallback, useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { IoIosArrowBack } from 'react-icons/io';
+
 const PostDetailPage = () => {
-  return <div>PostDetailPage</div>;
+  const [post, setPost] = useState<PostProps | null>(null);
+  const params = useParams();
+  const navigate = useNavigate();
+
+  const getPost = useCallback(async () => {
+    if (params.id) {
+      const docRef = doc(db, 'posts', params.id);
+      const docSnap = await getDoc(docRef);
+
+      setPost({ ...(docSnap?.data() as PostProps), id: docSnap?.id });
+    }
+  }, [params.id]);
+
+  useEffect(() => {
+    if (params.id) getPost();
+  }, [getPost, params.id]);
+
+  return (
+    <div className="post">
+      <header className="post__header">
+        <button className="button" onClick={() => navigate(-1)}>
+          <IoIosArrowBack className="post__header-btn" />
+        </button>
+      </header>
+      {post ? <PostBox post={post} /> : <Loader />}
+    </div>
+  );
 };
 
 export default PostDetailPage;
